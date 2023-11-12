02f03875841f622b78450d4bc9bfd92113d753fa
machleslie <machariales@gmail.com>
2023-11-12 15:54:24 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,13 +7,16 @@ function Form(props) {
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
   }
+  const handleSubmit = function handleSubmit(e) {
+    e.preventDefault();
+  };
 
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
